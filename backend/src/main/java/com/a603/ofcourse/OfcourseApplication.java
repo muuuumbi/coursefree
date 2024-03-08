473d7ebcfd620ff90738a4bcbb620850669ddc91
lkt9899 <lkt9899@naver.com>
2024-03-08 17:55:31 +0900

    init

---
+++ backend/src/main/java/com/a603/ofcourse/OfcourseApplication.java
@@ -0,0 +1,13 @@
+package com.a603.ofcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfcourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfcourseApplication.class, args);
+    }
+
+}
