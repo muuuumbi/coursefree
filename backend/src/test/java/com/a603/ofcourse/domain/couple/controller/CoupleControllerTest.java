1c9f339a22a70c1f1960bf51ab6133ee381b8008
muuuumbi <dnpfkdlvm123@gmail.com>
2024-03-25 09:03:47 +0900

    test: oauth, couple 테스트 코드 추가

--- backend/src/test/java/com/a603/ofcourse/domain/couple/controller/CoupleControllerTest.java
+++ backend/src/test/java/com/a603/ofcourse/domain/couple/controller/CoupleControllerTest.java
@@ -1,36 +1,80 @@
 package com.a603.ofcourse.domain.couple.controller;
 
 import com.a603.ofcourse.domain.couple.service.CoupleService;
+import com.a603.ofcourse.domain.oauth.service.JwtTokenService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(CoupleController.class)
+@ExtendWith(MockitoExtension.class)
 class CoupleControllerTest {
+    @Mock
+    private CoupleService coupleService;
+    @Mock
+    private JwtTokenService jwtTokenService;
 
-    @Autowired
+    @InjectMocks
+    private CoupleController coupleController;
+
     private MockMvc mockMvc;
 
-    @MockBean
-    private CoupleService coupleService;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void initMockMvc(){
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(coupleController)
+                .build();
+
+    }
 
     @Test
-    void connectCoupleWithMember() throws Exception{
-        //Given
-        String uuid = "bfcbec3366724859b697d89ede373da0";
-        String accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJfaWQiOjEsImlhdCI6MTcxMTA4NzY1MSwiZXhwIjoxNzExMDg3NjU1fQ.cZND5Nva7OuAYueXswgltDTnd7P9zaMBSGs2oZdjHsY";
+    @DisplayName("커플 연동 테스트")
+    void connectCoupleWithMemberTest() throws Exception {
+        // Given
+        String accessToken = "Bearer accessToken";
+        String uuid  = "uuid";
+        Integer visitorId = 2;
+        Integer inviterId = 1;
 
-        //when, then
+        Claims claims = Jwts.claims();
+        claims.put("member_id", visitorId);
+
+        //stub
+        // 액세스토큰에서 초대된 사람의 아이디 가져오기
+        BDDMockito.given(jwtTokenService.getPayload(accessToken))
+                .willReturn(claims);
+        // uuid를 이용해서 초대한 사람의 아이디 가져오기
+        BDDMockito.given(coupleService.getInviterIdFromLink(uuid))
+                        .willReturn(inviterId);
+        // When/Then
         mockMvc.perform(post("/connect/exist/{UUID}", uuid)
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", accessToken))
+                        .header("Authorization", accessToken)
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+
+        //제대로 호출되었는지 검증
+        BDDMockito.verify(coupleService).connectCouple(visitorId, inviterId);
     }
 }
