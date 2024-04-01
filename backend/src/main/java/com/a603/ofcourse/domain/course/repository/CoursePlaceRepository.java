9ccce9570741228569e5b0c8fbf6ba7e89ebffbb
박세헌 <quwe153@naver.com>
2024-04-01 12:11:50 +0900

    feat : 코스 아이디로 조회 함수 구현

--- backend/src/main/java/com/a603/ofcourse/domain/course/repository/CoursePlaceRepository.java
+++ backend/src/main/java/com/a603/ofcourse/domain/course/repository/CoursePlaceRepository.java
@@ -2,6 +2,15 @@ package com.a603.ofcourse.domain.course.repository;
 
 import com.a603.ofcourse.domain.course.domain.CoursePlace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CoursePlaceRepository extends JpaRepository<CoursePlace, Integer> {
+
+    @Query("select cp " +
+            "from CoursePlace cp " +
+            "join fetch cp.place " +
+            "where cp.course.id = :courseId")
+    List<CoursePlace> findByCourseId(Integer courseId);
 }
