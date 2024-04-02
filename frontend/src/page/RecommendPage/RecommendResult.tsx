45a28914b231aa549eaa7dc590ccfdec82289201
박희준 <wooryjoon@gmail.com>
2024-04-02 16:58:03 +0900

    Feat :

--- frontend/src/page/RecommendPage/RecommendResult.tsx
+++ frontend/src/page/RecommendPage/RecommendResult.tsx
@@ -1,7 +1,12 @@
+import { useLocation } from 'react-router-dom'
+
 import LongCourseInfo from '@component/Course/LongCourseInfo'
 import RecommendTopBar from '@component/layout/RecommendTopBar'
 
 export default function RecommendResult() {
+  const location = useLocation()
+  const courseId = location.state.courseId
+
   return (
     <>
       <RecommendTopBar />
