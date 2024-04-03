f78216e8224725b6c1afdc1ea900c395821cb8c0
wooryjoon <wooryjoon@gmail.com>
2024-04-04 04:37:07 +0900

    Feat : FE_padding_bottom수정

--- frontend/src/styled/component/pages/HomePage/ArticleGrid.tsx
+++ frontend/src/styled/component/pages/HomePage/ArticleGrid.tsx
@@ -4,5 +4,5 @@ import styled from '@emotion/styled'
 export const ArticleGridContainer = styled(SimpleGrid)`
   height: 100%;
   overflow-y: scroll;
-  padding-bottom: 2rem;
+  padding-bottom: 2.2rem;
 `
