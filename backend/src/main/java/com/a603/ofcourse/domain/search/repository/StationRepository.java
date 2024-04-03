2557fc401d950836aebeecfd3176136f2f71ed42
lkt9899 <lkt9899@naver.com>
2024-04-03 15:56:19 +0900

    feat : 자동 완성 고도화 - 영한 변환 - 오타 수정 - 초성 검색

--- backend/src/main/java/com/a603/ofcourse/domain/search/repository/StationRepository.java
+++ backend/src/main/java/com/a603/ofcourse/domain/search/repository/StationRepository.java
@@ -3,8 +3,5 @@ package com.a603.ofcourse.domain.search.repository;
 import com.a603.ofcourse.domain.search.document.Station;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-import java.util.List;
-
-public interface StationRepository extends ElasticsearchRepository<Station, String> {
-    List<Station> findByStationNameContainsIgnoreCase(String stationName);
+public interface StationRepository extends ElasticsearchRepository<Station, Integer>, CustomStationRepository {
 }
